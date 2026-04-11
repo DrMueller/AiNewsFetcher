e2d4027813f454c14c2988db2e593d4166032cfc
Müller Matthias <matthias.mueller@noser.com>
2026-04-11 20:30:20 +0200

    WIP

--- Pipelines/Releasing/Terraform/main.tf
+++ Pipelines/Releasing/Terraform/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.65"
+      version = "~> 3.0"
     }
   }
-  # The values are needed for local commands like creating the workspaces
+
   backend "azurerm" {
     resource_group_name  = "matthias"
     storage_account_name = "terraformstuff"
@@ -17,42 +17,53 @@ terraform {
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = "d6fddda6-f690-4755-92c2-f22a3521bab0"
-  features {
-  }
+
+  features {}
 }
 
-resource "azurerm_app_service_plan" "res" {
-  name                = lower(substr(join("", [var.environment_prefix, var.app_name]), 0, 24))
+locals {
+  base_name = substr(
+    lower(replace("${var.environment_prefix}${var.app_name}", "/[^a-z0-9]/", "")),
+    0,
+    24
+  )
+}
+
+resource "azurerm_service_plan" "res" {
+  name                = local.base_name
   location            = var.region
   resource_group_name = var.resource_group_name
-  kind                = "FunctionApp"
 
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+  os_type  = "Windows"
+  sku_name = "Y1" # Consumption Plan für Functions
 }
 
 resource "azurerm_storage_account" "res" {
-  name                     = lower(substr(join("", [var.environment_prefix, var.app_name]), 0, 24))
+  name                     = local.base_name
   resource_group_name      = var.resource_group_name
   location                 = var.region
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_function_app" "res" {
-  name                       = var.app_name
-  location                   = var.region
-  resource_group_name        = var.resource_group_name
-  app_service_plan_id        = azurerm_app_service_plan.res.id
+resource "azurerm_windows_function_app" "res" {
+  name                = local.base_name
+  location            = var.region
+  resource_group_name = var.resource_group_name
+  service_plan_id     = azurerm_service_plan.res.id
+
   storage_account_name       = azurerm_storage_account.res.name
   storage_account_access_key = azurerm_storage_account.res.primary_access_key
+
   site_config {
-    dotnet_framework_version = "v6.0"
+    application_stack {
+      dotnet_version = "v6.0"
+    }
   }
-    app_settings = {
-    "AppName"    = var.app_name
+
+  app_settings = {
+    "AppName"                = var.app_name
+    "AzureWebJobsStorage"   = azurerm_storage_account.res.primary_connection_string
+    "FUNCTIONS_WORKER_RUNTIME" = "dotnet"
   }
-  version = "~4"
 }
