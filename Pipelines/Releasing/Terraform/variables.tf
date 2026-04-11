0ebe20c4e34c80bf9dda583fd1f3cb3b7aac47b5
Müller Matthias <matthias.mueller@noser.com>
2026-04-11 18:11:38 +0200

    WIP

--- Pipelines/Releasing/Terraform/variables.tf
+++ Pipelines/Releasing/Terraform/variables.tf
@@ -8,7 +8,9 @@ variable "subscription_id" {
   default = "91660754-3529-407f-8458-92759935fbf7"
 }
 
-variable "app_name" {}
+variable "app_name" {
+  type = string
+}
 
 variable "resource_group_name" {
   type    = string
