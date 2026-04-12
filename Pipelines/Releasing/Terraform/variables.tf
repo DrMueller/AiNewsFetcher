12260ae2f83ac22fbd32bd65891f706622cb8d93
Müller Matthias <matthias.mueller@noser.com>
2026-04-12 16:46:49 +0200

    WIP

--- Pipelines/Releasing/Terraform/variables.tf
+++ Pipelines/Releasing/Terraform/variables.tf
@@ -8,6 +8,11 @@ variable "subscription_id" {
   default = "91660754-3529-407f-8458-92759935fbf7"
 }
 
+variable "tenant_id" {
+  type    = string
+  default = "d6fddda6-f690-4755-92c2-f22a3521bab0"
+}
+
 variable "storage_account_name" {
   type = string
   default = "matthiasstorage"
