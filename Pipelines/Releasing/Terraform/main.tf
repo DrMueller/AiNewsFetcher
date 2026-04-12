388a522b957db66d1c0d20930a81e79df4afd9e1
Müller Matthias <matthias.mueller@noser.com>
2026-04-12 16:38:50 +0200

    WIP

--- Pipelines/Releasing/Terraform/main.tf
+++ Pipelines/Releasing/Terraform/main.tf
@@ -1,61 +1,84 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      source  = "azurerm"
+      version = "4.58.0"
     }
   }
-
+  # The values are needed for local commands like creating the workspaces
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = "terraformstuff"
-    container_name       = "terraformstuffblob"
+    resource_group_name  = "TerraformStuff"
+    storage_account_name = "terraformstuffblob"
+    container_name       = "terraformstuffblobcontainer"
     key                  = "tf/terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   subscription_id = var.subscription_id
-  tenant_id       = "d6fddda6-f690-4755-92c2-f22a3521bab0"
-
-  features {}
-}
-
-resource "azurerm_service_plan" "res" {
-  name                = var.app_name
-  location            = var.region
-  resource_group_name = var.resource_group_name
-
-  os_type  = "Windows"
-  sku_name = "Y1" # Consumption Plan für Functions
-}
-
-data "azurerm_storage_account" "existing" {
-  name                = var.storage_account_name
-  resource_group_name = var.resource_group_name
-}
-
-resource "azurerm_windows_function_app" "res" {
-  name                = var.app_name
-  location            = var.region
-  resource_group_name = var.resource_group_name
-  service_plan_id     = azurerm_service_plan.res.id
-
-  storage_account_name       = data.azurerm_storage_account.existing.name
-  storage_account_access_key = data.azurerm_storage_account.existing.primary_access_key
-
-  site_config {
-    application_insights_connection_string =  var.app_insights_connection_string
-    application_stack {
-      dotnet_version = "v6.0"
-    }
+  tenant_id       = var.tenant_id
+  features {
   }
+}
 
-  app_settings = {
-    "AppName"                      = var.app_name
-    "FUNCTIONS_WORKER_RUNTIME"     = "dotnet-isolated"
-    "FUNCTIONS_EXTENSION_VERSION"  = "~4"
-    "AzureWebJobsStorage"          = data.azurerm_storage_account.existing.primary_connection_string
-    "WEBSITE_RUN_FROM_PACKAGE"     = "1"
+resource "azurerm_function_app_flex_consumption" "res-0" {
+  app_settings                       = {}
+  client_certificate_enabled         = false
+  client_certificate_exclusion_paths = ""
+  client_certificate_mode            = "Required"
+  custom_domain_verification_id      = "" # Masked sensitive attribute
+  enabled                            = true
+  http_concurrency                   = 0
+  https_only                         = true
+  instance_memory_in_mb              = 512
+  location                           = var.region
+  maximum_instance_count             = 100
+  name                               = var.app_name
+  public_network_access_enabled      = true
+  resource_group_name                = var.resource_group_name
+  runtime_name                       = "dotnet-isolated"
+  runtime_version                    = "10.0"
+  service_plan_id                    = "/subscriptions/91660754-3529-407f-8458-92759935fbf7/resourceGroups/matthias/providers/Microsoft.Web/serverFarms/ASP-Matthias-adaa"
+  storage_access_key                 = ""
+  storage_authentication_type        = "StorageAccountConnectionString"
+  storage_container_endpoint         = "https://matthiasstorage.blob.core.windows.net/app-package-ainewsfetcher-0809eed"
+  storage_container_type             = "blobContainer"
+  storage_user_assigned_identity_id  = ""
+  tags = {
+    "hidden-link: /app-insights-resource-id" = "/subscriptions/91660754-3529-407f-8458-92759935fbf7/resourceGroups/Matthias/providers/microsoft.insights/components/MatthiasAppInsights"
+  }
+  virtual_network_subnet_id                      = ""
+  webdeploy_publish_basic_authentication_enabled = false
+  zip_deploy_file                                = ""
+  site_config {
+    api_definition_url                            = ""
+    api_management_api_id                         = ""
+    app_command_line                              = ""
+    application_insights_connection_string        = var.app_insights_connection_string
+    container_registry_managed_identity_client_id = ""
+    container_registry_use_managed_identity       = false
+    default_documents                             = ["Default.htm", "Default.html", "Default.asp", "index.htm", "index.html", "iisstart.htm", "default.aspx", "index.php"]
+    elastic_instance_minimum                      = 0
+    health_check_eviction_time_in_min             = 0
+    health_check_path                             = ""
+    http2_enabled                                 = false
+    ip_restriction_default_action                 = ""
+    load_balancing_mode                           = "LeastRequests"
+    managed_pipeline_mode                         = "Integrated"
+    minimum_tls_version                           = "1.2"
+    remote_debugging_enabled                      = false
+    remote_debugging_version                      = "VS2022"
+    runtime_scale_monitoring_enabled              = false
+    scm_ip_restriction_default_action             = ""
+    scm_minimum_tls_version                       = "1.2"
+    scm_use_main_ip_restriction                   = false
+    use_32_bit_worker                             = false
+    vnet_route_all_enabled                        = false
+    websockets_enabled                            = false
+    worker_count                                  = 1
+    cors {
+      allowed_origins     = ["https://portal.azure.com"]
+      support_credentials = false
+    }
   }
 }
