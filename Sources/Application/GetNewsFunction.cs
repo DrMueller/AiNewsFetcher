1b1dcdcd2cd9cdf5e2b937c1af0bc89c6e0bcc42
Müller Matthias <matthias.mueller@noser.com>
2026-04-16 20:14:10 +0200

    WIP

--- Sources/Application/GetNewsFunction.cs
+++ Sources/Application/GetNewsFunction.cs
@@ -1,6 +1,8 @@
 using AiNewsFetcher.Infrastructure.AzureOpenAi;
 using AiNewsFetcher.Infrastructure.Emails.Models;
 using AiNewsFetcher.Infrastructure.Emails.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using OpenAI.Chat;
@@ -11,11 +13,23 @@ public class GetNewsFunction(ILoggerFactory loggerFactory, IAzureOpenAiClient ai
 {
     private readonly ILogger _logger = loggerFactory.CreateLogger<GetNewsFunction>();
 
+
+    [Function("TestGetNewsFunction")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+
     [Function("GetNewsFunction")]
     public async Task Run([TimerTrigger("0 0 23 * * *")] TimerInfo myTimer)
     {
         _logger.LogInformation("C# Timer trigger function executed at: {executionTime}", DateTime.Now);
+        await RunInternalAsync()
+    }
 
+    private async Task RunInternalAsync()
+    {
         var message = new SystemChatMessage(
             ChatMessageContentPart.CreateTextPart(Prompts.SearchPrompt
             ));
